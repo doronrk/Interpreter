1a71727e385688b2684ace0ed9b07ba3961a226d
Doron <doronr@sas.upenn.edu>
2013-06-11 15:57:34 -0400

    Made progress on environment and lookup

--- interpreter.hs
+++ interpreter.hs
@@ -1,10 +1,11 @@
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+--{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 
 data Expr = 
 	  Numb Int
 	| Plus Expr Expr
 	| Minus Expr Expr
 	| Times Expr Expr
+	| Divide Expr Expr
 
 eval :: Expr -> Int
 eval e = case e of 
@@ -12,3 +13,15 @@ eval e = case e of
 	Plus e1 e2 -> eval e1 + eval e2
 	Minus e1 e2 -> eval e1 - eval e2
 	Times e1 e2 -> eval e1 * eval e2
+	Divide e1 e2 -> eval e1 `div` eval e2
+
+
+type Env = [(String, Expr)]
+
+lookup' :: Env -> String -> Bool
+--lookup [] _ = error "oh no, unreferenced variable"
+lookup' (x:xs) v = (fst x) == v
+
+contains' :: [Int] -> Int -> Bool
+contains' [] _ = False
+contains' (x:xs) y = x==y || contains' xs y
