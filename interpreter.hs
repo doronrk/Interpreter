14dbd45c0aef07b03a88f6a6231ca60e21b85098
Doron <doronr@sas.upenn.edu>
2013-06-12 13:10:51 -0400

    eval now returns expr instead of int, allowing functions to remain functions when evaluated

--- interpreter.hs
+++ interpreter.hs
@@ -12,21 +12,26 @@ data Expr =
 	| Let String Expr Expr
 	| Func String Expr
 	| CallF Expr Expr
-	deriving (Show)
+	deriving (Show, Eq)
 
-eval :: Expr -> Env -> Int
-eval e env = case e of 
-	Numb x -> x
-	Plus e1 e2 -> eval e1 env + eval e2 env
-	Minus e1 e2 -> eval e1 env - eval e2 env
-	Times e1 e2 -> eval e1 env * eval e2 env
-	Divide e1 e2 -> eval e1 env `div` eval e2 env
+eval :: Expr -> Env -> Expr
+eval e env =
+	let
+		handleArith op (Numb arg1) (Numb arg2) = Numb (op arg1 arg2)
+		handleArith _ _ _ = error "tried to apply arith op to non-number"
+	in
+	case e of 
+	Numb x -> Numb x
+	Plus e1 e2 -> handleArith (+) (eval e1 env) (eval e2 env)
+	Minus e1 e2 -> handleArith (-) (eval e1 env) (eval e2 env)
+	Times e1 e2 -> handleArith (*) (eval e1 env) (eval e2 env)
+	Divide e1 e2 -> handleArith (div) (eval e1 env) (eval e2 env)
 	Variable s -> eval (getExpr s env) env
 	Let s e1 e2 -> eval e2 (addVar s e1 env)
-	CallF f arg -> case f of
+	Func var body -> Func var body
+	CallF f arg -> case eval f env of
 					Func s body -> eval body (addVar s arg env)
 					_ -> error "tried to call non-function"
-	Func s e1 -> error "Not implented yet!"
 	-- 2a) we need env in each call to eval since there may be variables nested within Expr's
 	-- 2b) however, is there a way to avoid writing it in every recursive line since the Env doesn't change as we eval? 
 	-- 2c) (at least, I don't think the Env changes since variables are assigned during parsing, correct?)
@@ -45,13 +50,13 @@ myEnv = addVar "a" (Numb 1) []
 myEnv2 = addVar "b" (Numb 2) myEnv
 myEnv3 = addVar "three" (Plus (Numb 2) (Numb 1)) myEnv2
 
-testNumb0 = TestCase (assertEqual "Numb 0" 0 (eval (Numb 0) []))
-testNumb1 = TestCase (assertEqual "Numb 1" 1 (eval (Numb 1) []))
-testPlus = TestCase (assertEqual "Plus (Numb 1) (Numb 2)" 3 (eval (Plus (Numb 1) (Numb 2)) []))
-testEnv = TestCase (assertEqual "Variable a 4" 4 (eval (Variable "a") [("a", Numb 4)]))
-testLet = TestCase (assertEqual "Let" 2 (eval (Let "x" (Numb 1) (Plus (Variable "x") (Variable "x"))) []))
-testLet1 = TestCase (assertEqual "nested Let" 3 (eval (Let "x" (Numb 1) (Let "y" (Numb 2) (Plus (Variable "x") (Variable "y")))) []))
-testCallF1 = TestCase (assertEqual "" 4 (eval (CallF (Func "x" (Plus (Variable "x") (Numb 1) )) (Numb 3)) []))
+testNumb0 = TestCase (assertEqual "Numb 0" (Numb 0) (eval (Numb 0) []))
+testNumb1 = TestCase (assertEqual "Numb 1" (Numb 1) (eval (Numb 1) []))
+testPlus = TestCase (assertEqual "Plus (Numb 1) (Numb 2)" (Numb 3) (eval (Plus (Numb 1) (Numb 2)) []))
+testEnv = TestCase (assertEqual "Variable a 4" (Numb 4) (eval (Variable "a") [("a", Numb 4)]))
+testLet = TestCase (assertEqual "Let" (Numb 2) (eval (Let "x" (Numb 1) (Plus (Variable "x") (Variable "x"))) []))
+testLet1 = TestCase (assertEqual "nested Let" (Numb 3) (eval (Let "x" (Numb 1) (Let "y" (Numb 2) (Plus (Variable "x") (Variable "y")))) []))
+testCallF1 = TestCase (assertEqual "call expr" (Numb 4) (eval (CallF (Func "x" (Plus (Variable "x") (Numb 1) )) (Numb 3)) []))
 
 tests = TestList [testNumb0, testNumb1, testPlus, testEnv, testLet, testLet1, testCallF1]
 
