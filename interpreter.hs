276e730f23244480b6f451f247c263aaf85d144f
aki <spacemanaki@gmail.com>
2013-06-14 18:10:26 -0400

    fixed dynamic scope issue, implemented lexical scope

--- interpreter.hs
+++ interpreter.hs
@@ -11,7 +11,7 @@ data Expr =
 	| Divide Expr Expr
 	| Variable String
 	| Let String Expr Expr
-	| Func String Expr
+	| Func Env String Expr
 	| CallF Expr Expr
 	| If Expr Expr Expr
 
@@ -31,9 +31,9 @@ eval e env =
 		Divide e1 e2 -> handleArith (div) (eval e1 env) (eval e2 env)
 		Variable s -> eval (getExpr s env) env
 		Let s e1 e2 -> eval e2 (addVar s e1 env)
-		Func var body -> Func var body
+		Func _ var body -> Func env var body
 		CallF f arg -> case eval f env of
-						Func s body -> eval body (addVar s arg env)
+						Func cenv s body -> eval body (addVar s arg cenv)
 						_ -> error "tried to call non-function"
 		If boolean true false -> case eval boolean env of
 						Boolean True -> eval true env
@@ -72,29 +72,23 @@ testDivide = TestCase (assertEqual "Divide (Numb 12) (Numb 4)" (Numb 3) (eval (D
 testEnv = TestCase (assertEqual "Variable a 4" (Numb 4) (eval (Variable "a") [("a", Numb 4)]))
 testLet = TestCase (assertEqual "Let" (Numb 2) (eval (Let "x" (Numb 1) (Plus (Variable "x") (Variable "x"))) []))
 testLet1 = TestCase (assertEqual "nested Let" (Numb 3) (eval (Let "x" (Numb 1) (Let "y" (Numb 2) (Plus (Variable "x") (Variable "y")))) []))
-testCallF1 = TestCase (assertEqual "call expr" (Numb 4) (eval (CallF (Func "x" (Plus (Variable "x") (Numb 1) )) (Numb 3)) []))
+testCallF1 = TestCase (assertEqual "call expr" (Numb 4) (eval (CallF (Func [] "x" (Plus (Variable "x") (Numb 1) )) (Numb 3)) []))
 
-timesTwo = Func "x" (Times (Numb 2) (Variable "x")) -- lambda x = 2*x
+timesTwo = Func [] "x" (Times (Numb 2) (Variable "x")) -- lambda x = 2*x
 myCallF = CallF timesTwo(Numb 4)
 
 testCallF2 = TestCase (assertEqual "jawn" (Numb 8) (eval (myCallF) []))
 
 testIf = TestCase (assertEqual "if" (Numb 0) (eval (If (Boolean True) (Numb 0) (Numb 1)) []))
 
-
-
 testClosure = TestCase (assertEqual "closure" (Numb 11) (eval
 	(CallF
 		(CallF
-			(Func "x"
-				(Func "y" (Plus (Variable "x") (Variable "y"))))
+			(Func [] "x"
+				(Func [] "y" (Plus (Variable "x") (Variable "y"))))
 			(Numb 5))
 		(Numb 6)) []))
 
 tests = TestList [testNumb0, testNumb1, testPlus, testMinus, testTimes, testDivide, testEnv, testLet, testLet1, testCallF1, testCallF2, testBoolean, testIf, testClosure]
 
 main = do runTestTT tests
-
-code = " asdf asdf l;kj ;lkj j; "
-newcode = words code
-
