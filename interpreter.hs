748edf57ecb1188bad13e8a6c4342f56f9838ee9
aki <spacemanaki@gmail.com>
2013-06-17 15:32:31 -0400

    added alternative Show instance for Expr

--- interpreter.hs
+++ interpreter.hs
@@ -14,29 +14,45 @@ data Expr =
 	| CallF Expr Expr
 	| If Expr Expr Expr
 
-	deriving (Show, Eq)
+	deriving (Eq)
 
-eval :: Expr -> Env -> Expr
-eval e env =
+instance Show Expr where
+  show (Numb n) = show n
+  show (Boolean b) = show b
+  show (Plus e1 e2) = "#+ " ++ show e1 ++ " " ++ show e2
+  show (Minus e1 e2) = "#- " ++ show e1 ++ " " ++ show e2
+  show (Times e1 e2) = "#* " ++ show e1 ++ " " ++ show e2
+  show (Divide e1 e2) = "#/ " ++ show e1 ++ " " ++ show e2
+  show (Variable v) = v
+  show (Let v e1 e2) = "#<let " ++ v ++ "=" ++ show e1 ++ " in " ++ show e2 ++ ">"
+  show (Func env v e) = "#<fn " ++ v ++ " => " ++ show e ++ ">"
+  show (CallF e1 e2) = "#<app " ++ show e1 ++ " " ++ show e2 ++ ">"
+  show (If e1 e2 e3) = "#<if " ++ show e1 ++ "," ++ show e2 ++ "," ++ show e3 ++ ">"
+
+eval :: Expr -> Expr
+eval e = eval' e []
+
+eval' :: Expr -> Env -> Expr
+eval' e env =
 	let
 		handleArith op (Numb arg1) (Numb arg2) = Numb (op arg1 arg2)
 		handleArith _ _ _ = error "tried to apply arith op to non-number"
 	in case e of 
 		Numb x -> Numb x
 		Boolean x -> Boolean x
-		Plus e1 e2 -> handleArith (+) (eval e1 env) (eval e2 env)
-		Minus e1 e2 -> handleArith (-) (eval e1 env) (eval e2 env)
-		Times e1 e2 -> handleArith (*) (eval e1 env) (eval e2 env)
-		Divide e1 e2 -> handleArith (div) (eval e1 env) (eval e2 env)
-		Variable s -> eval (getExpr s env) env
-		Let s e1 e2 -> eval e2 (addVar s e1 env)
+		Plus e1 e2 -> handleArith (+) (eval' e1 env) (eval' e2 env)
+		Minus e1 e2 -> handleArith (-) (eval' e1 env) (eval' e2 env)
+		Times e1 e2 -> handleArith (*) (eval' e1 env) (eval' e2 env)
+		Divide e1 e2 -> handleArith (div) (eval' e1 env) (eval' e2 env)
+		Variable s -> eval' (getExpr s env) env
+		Let s e1 e2 -> eval' e2 (addVar s e1 env)
 		Func _ var body -> Func env var body
-		CallF f arg -> case eval f env of
-						Func cenv s body -> eval body (addVar s arg cenv)
+		CallF f arg -> case eval' f env of
+						Func cenv s body -> eval' body (addVar s arg cenv)
 						_ -> error "tried to call non-function"
-		If boolean true false -> case eval boolean env of
-						Boolean True -> eval true env
-						Boolean False -> eval false env
+		If boolean true false -> case eval' boolean env of
+						Boolean True -> eval' true env
+						Boolean False -> eval' false env
 						_ -> error "non-boolean in if"
 
 type Env = [(String, Expr)]
