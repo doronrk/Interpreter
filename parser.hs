76ffd7993bc1c0b0745be26a94fbd1f24f8d9cf2
aki <spacemanaki@gmail.com>
2013-06-16 22:25:39 -0400

    removed 'AST_' from AST datatype variants, for now

--- parser.hs
+++ parser.hs
@@ -4,7 +4,6 @@ import Data.Char (isDigit)
 import Test.HUnit
 
 -- grammar:
--- expr   -> ( expr )
 -- expr   -> term + expr
 -- expr   -> term - expr
 -- term   -> factor term'
@@ -14,13 +13,14 @@ import Test.HUnit
 -- factor -> int
 -- factor -> bool
 -- factor -> id
+-- factor -> ( expr )
 
-data AST = AST_Bool Bool
-         | AST_Num Int
-         | AST_Id String
-         | AST_Add AST AST
-         | AST_Mul AST AST
-         | AST_Div AST AST
+data AST = Bool Bool
+         | Num Int
+         | Id String
+         | Add AST AST
+         | Mul AST AST
+         | Div AST AST
          deriving (Show, Eq)
 
 expr = do token (char '(')
@@ -31,11 +31,11 @@ expr = do token (char '(')
 
 ast_num :: Parser AST
 ast_num = do n <- token nat
-             return (AST_Num n)
+             return (Num n)
 
 ast_bool :: Parser AST
 ast_bool = do b <- false +++ true
-              return (AST_Bool b)
+              return (Bool b)
            where false = do symbol "false"
                             return False
                  true = do symbol "true"
@@ -43,7 +43,7 @@ ast_bool = do b <- false +++ true
 
 ast_id :: Parser AST
 ast_id = do id <- identifier
-            return (AST_Id id)
+            return (Id id)
 
 factor :: Parser AST
 factor = ast_num +++ ast_bool +++ ast_id
@@ -54,16 +54,16 @@ term = factor >>= term'
 term' :: AST -> Parser AST
 term' lhs = (do op <- symbol "*" +++ symbol "/"
                 rhs <- factor
-                (term' ((if op == "*" then AST_Mul else AST_Div) lhs rhs)))
+                (term' ((if op == "*" then Mul else Div) lhs rhs)))
             +++ return lhs
 
 testExpr = TestCase (assertEqual "expr" [("foo","")] (parse expr "(foo)"))
-testNum = TestCase (assertEqual "num" [(AST_Num 123,"456")] (parse factor "123 456"))
-testBool = TestCase (assertEqual "bool" [(AST_Bool True,"456")] (parse factor "true 456"))
-testId = TestCase (assertEqual "id" [(AST_Id "foo","456")] (parse factor "foo 456"))
-testMul = TestCase (assertEqual "mul" [(AST_Mul (AST_Num 1) (AST_Num 2), "")] (parse term "1 * 2"))
-testMulLeftAssoc = TestCase (assertEqual "mul should be left assoc" [(AST_Mul (AST_Mul (AST_Num 1) (AST_Num 2)) (AST_Num 3), "")] (parse term "1 * 2 * 3"))
-testMulDivLeftAssoc = TestCase (assertEqual "mul & div should be left assoc" [(AST_Div (AST_Mul (AST_Num 1) (AST_Num 2)) (AST_Num 3), "")] (parse term "1 * 2 / 3"))
+testNum = TestCase (assertEqual "num" [(Num 123,"456")] (parse factor "123 456"))
+testBool = TestCase (assertEqual "bool" [(Bool True,"456")] (parse factor "true 456"))
+testId = TestCase (assertEqual "id" [(Id "foo","456")] (parse factor "foo 456"))
+testMul = TestCase (assertEqual "mul" [(Mul (Num 1) (Num 2), "")] (parse term "1 * 2"))
+testMulLeftAssoc = TestCase (assertEqual "mul should be left assoc" [(Mul (Mul (Num 1) (Num 2)) (Num 3), "")] (parse term "1 * 2 * 3"))
+testMulDivLeftAssoc = TestCase (assertEqual "mul & div should be left assoc" [(Div (Mul (Num 1) (Num 2)) (Num 3), "")] (parse term "1 * 2 / 3"))
 
 tests = TestList [testExpr,testNum,testBool,testId,testMul,testMulLeftAssoc,testMulDivLeftAssoc]
 
